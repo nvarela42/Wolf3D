e516d347f0bfc5310234b44022c35e37ac22f1e1
agfernan <agfernan@student.42.fr>
2017-10-18 14:25:29 +0200

    added .gitignore

--- srcs/start_wolf.c
+++ srcs/start_wolf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   start_wolf.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nvarela <nvarela@student.42.fr>            +#+  +:+       +#+        */
+/*   By: agfernan <agfernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/18 12:13:52 by nvarela           #+#    #+#             */
-/*   Updated: 2017/10/18 14:00:46 by nvarela          ###   ########.fr       */
+/*   Updated: 2017/10/18 14:21:46 by agfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void		start_wolf(t_env *env)
 {
-	MLX.image = mlx_new_image(&(MLX.mlx), WIN_W, WIN_H);
-	// mlx_get_data_addr(MLX.image, &(MLX.mlximgbpp), &(MLX.mlximgsize), &(MLX.mlximgendian));
-	mlx_put_image_to_window(&(MLX.mlx), &(MLX.win), &(MLX.image), 0, 0);
+	MLX.image = mlx_new_image(MLX.mlx, WIN_W, WIN_H);
+	MLX.imgchar = mlx_get_data_addr(MLX.image, &(MLX.mlximgbpp), &(MLX.mlximgsize), &(MLX.mlximgendian));
+	mlx_put_image_to_window(MLX.mlx, MLX.win, MLX.imgchar, 0, 0);
 	printf("ok1\n");
-	// mlx_hook(&(MLX.win), 17, 0, quit_cross, &(MLX.mlx));
+	mlx_hook(&(MLX.win), 17, 0, quit_cross, env);
 	printf("ok2\n");
-	// mlx_key_hook(&(MLX.win), key_fonction, &(MLX.mlx));
+	mlx_key_hook(&(MLX.win), key_fonction, env);
 	printf("ok3\n");
-	// mlx_mouse_hook(&(MLX.win), /*mouse_button*/, &mlx);
+	// mlx_mouse_hook(&(MLX.win), /*mouse_button*/, env);
 	mlx_loop(&(MLX.mlx));
 }
